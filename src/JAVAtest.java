5c26065c94c17ea875e2380221bf53cae67f99ba
Daniel.Laga <daniellaga84@gmail.com>
2020-08-14 20:04:53 +0200

    Initial repository

---
+++ src/JAVAtest.java
@@ -0,0 +1,30 @@
+public class JAVAtest {
+    public static void main(String[] args) {
+
+            int day;
+            int month;
+            int year;
+            day = 12;
+            month = 3;
+            year = 1984;
+            System.out.println(day + " . " + month + " . " + year);
+
+
+        String firstName = "Daniel ";
+        String lastName = "Laga";
+        String fullName = firstName+lastName;
+        System.out.println("Hello, my name is " + fullName+".");
+        int lettersInName = firstName.length() + lastName.length();
+        System.out.println("There are "+ lettersInName + " letters in my name.");
+
+
+
+
+        double fahrenheit = 99.0;
+        double celcius;
+        celcius = (fahrenheit -32) * 5 / 9;
+        System.out.println(celcius);
+    }
+}
+
+
